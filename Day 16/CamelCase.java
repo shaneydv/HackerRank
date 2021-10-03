69041b2c8d5ffbcbcddf7b006607313bb57ba6c7
Sushane <sushaneyadav@gmail.com>
2021-10-03 18:29:46 +0530

    ques 3 added

---
+++ Day 16/CamelCase.java
@@ -0,0 +1,44 @@
+/* Link: https://www.hackerrank.com/challenges/camelcase/problem */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int camelcase(String s) {
+    int cnt=1;
+    for(int i=0; i<s.length(); i++) {
+        if(Character.isUpperCase(s.charAt(i))){
+            cnt++;
+        }
+    }
+    return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
