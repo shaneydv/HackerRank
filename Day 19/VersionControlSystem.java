919feef13afe2f94759f713e0cbf6a8fe968c286
Sushane <sushaneyadav@gmail.com>
2021-12-20 20:08:18 +0530

    ques 3 added

--- Day 19/VersionControlSystem.java
+++ Day 19/VersionControlSystem.java
@@ -1,23 +1,9 @@
-/* package codechef; // don't place package name! */
+/* Link: https://www.codechef.com/problems/VCS/ */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        /* package codechef; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -60,30 +46,6 @@ class Codechef
                 }
             }
             System.out.println(c1 + " " + c2);
-        }
-	}
-	
-	public static boolean isPresent(int[] array, int target) {
-        int start = 0;
-        int end = array.length - 1;
-
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (target < array[mid]) {
-                end = mid - 1;
-            } else if (target > array[mid]) {
-                start = mid + 1;
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-}
-
-            
         }
 	}
 	
